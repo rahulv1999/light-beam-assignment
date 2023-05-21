efb8eb1ee341881befe05142f74f6146443b124a
RahulVerma <rahul.verma@increff.com>
2023-05-22 00:03:05 +0530

    change main.py output

--- DirectoryTree/src/main/config.py
+++ DirectoryTree/src/main/config.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append("../")
 sys.path.append("../../")
 
+
 from  src.main import folder
 
 class Config:
--- DirectoryTree/src/main/main.py
+++ DirectoryTree/src/main/main.py
@@ -1,18 +1,24 @@
-from config import Config
+import sys
+sys.path.append("../")
+sys.path.append("../../")
+from src.main import config
 
-root = Config("root")
+root = config.Config("root")
 root.add("root", "f1")
 root.add("root", "f2")
 root.add("root", "f3")
 root.add("root/f2", "f21")
 root.add("root/f2", "f22")
 root.print()
-# root.remove("f2")
-# root.add("root/f7/f1", "f2")
-root.print()
+print("************************")
 print(root.fetch("f21"))
 root.update("f21", "f221")
 root.print()
+print("************************")
+root.remove("f2")
+root.print()
+print("************************")
+
     
 
     
